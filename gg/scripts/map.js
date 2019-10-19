574809abe23b5c1badf24c2677d03f27f5bc9648
Randy Garcia <emil9669@esri.com>
2019-10-19 09:37:32 -0700

    added scene

---
+++ gg/scripts/map.js
@@ -0,0 +1,13 @@
+require(["esri/Map", "esri/views/SceneView"], function(Map, SceneView) {
+    var map = new Map({
+      basemap: "streets",
+      ground: "world-elevation"
+    });
+
+    var view = new SceneView({
+      container: "viewDiv",
+      map: map,
+      scale: 50000000,
+      center: [-101.17, 21.78]
+    });
+  });
